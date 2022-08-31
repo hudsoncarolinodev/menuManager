91176eef71146f1414d390404a47adecbe28f16b
Hudson Carolino <hudson@kenzie.com.br>
2022-08-30 22:58:45 -0300

    Adding jwt makin user token

--- src/controllers/users/createUser.controller.js
+++ src/controllers/users/createUser.controller.js
@@ -1,10 +1,10 @@
 import createUserService from '../../services/users/createUser.service'
 
-const createUserController = (req, res) => {
+const createUserController = async (req, res) => {
     
     const {name, email, password} = req.body
 
-    const user = createUserService(name, email, password)
+    const user = await createUserService(name, email, password)
 
     return res.status(201).json(user)
 }
--- src/services/users/loginUser.service.js
+++ src/services/users/loginUser.service.js
@@ -1,6 +1,24 @@
 import users from "./../../database"
+import  jwt  from "jsonwebtoken"
+import * as bcrypt from 'bcryptjs/dist/bcrypt'
 
-const createUser = (email, senha) => {
+const userLoginService = (email, password) => {
+    
+    const user =  users.find((user)=> user.email === email)
 
+    if(!user){
+        return "Invalid email or password"
+    }
 
+    const passwordMatch = bcrypt.compareSync(password, user.password)
+
+    if(!passwordMatch){
+        return "Invalid email or password"
+    }
+
+    const token = jwt.sign({email: email}, "SECRET_KEY", {expiresIn: "24h"})
+
+    return token
 }
+
+export default userLoginService
